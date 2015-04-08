7d57cf074fb7ac4d6b6b47573668a16fe9caa673
TimInghamDempster <tim_ingham_dempster@hotmail.co.uk>
2015-04-08 16:41:19 +0100

    [TID] Load CHASTE data files and draw the cells at the correct positions.

--- GameWorld/CellVisualiser.cs
+++ GameWorld/CellVisualiser.cs
@@ -18,10 +18,19 @@ namespace GameWorld
 
         Colour[] m_baseColours;
 
+        List<List<Vector3d>> m_positions;
+
+        const float Scale = 1000.0f;
+
+        int countdown = 0;
+        int activeList = 0;
+
         public CellVisualiser(IRenderer renderer)
         {
             m_renderer = renderer;
             m_scene = m_renderer.GetNewScene();
+
+            m_positions = new List<List<Vector3d>>();
 
             m_random = new Random();
 
@@ -48,10 +57,74 @@ namespace GameWorld
             m_baseColours[09] = new Colour() { R = 0.5f, G = 1.0f, B = 1.0f, A = 0.0f };
             m_baseColours[10] = new Colour() { R = 1.0f, G = 0.5f, B = 1.0f, A = 0.0f };
 
+            LoadFile("C:/Users/Tim/Desktop/UbuntuScratch/chasteOutput/OverlappingSpheres/results_from_time_0/results.viznodes");
+        }
+
+        public void LoadFile(string path)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(path, Encoding.UTF8);
+            string line = file.ReadLine();
+
+            while (line != null)
+            {
+                string[] entries = line.Split('\t');
+                string[] components = entries[1].Split(' ');
+                List<Vector3d> vectorList = new List<Vector3d>();
+
+                int numVectors = (components.Count() - 1) / 3;
+
+                for (int vectorId = 0; vectorId < numVectors; vectorId++)
+                {
+                    int componentId = vectorId * 3;
+
+                    float x, y, z;
+                    bool success = float.TryParse(components[componentId], out x);
+                    success = float.TryParse(components[componentId + 1], out y);
+                    success = float.TryParse(components[componentId + 2], out z);
+
+                    if (success)
+                    {
+                        Vector3d newVec = new Vector3d(x * Scale, y * Scale, z * Scale);
+                        vectorList.Add(newVec);
+                    }
+                }
+
+                m_positions.Add(vectorList);
+                line = file.ReadLine();
+            }
+
+            file.Close();
+        }
+
+        void SwitchActiveList(int newActive)
+        {
+            m_renderArrays.Positions = m_positions[newActive];
+            while (m_renderArrays.Colours.Count < m_positions[newActive].Count)
+            {
+                m_renderArrays.Colours.Add(m_baseColours[0]);
+            }
+            while (m_renderArrays.Colours.Count > m_positions[newActive].Count)
+            {
+                m_renderArrays.Colours.RemoveAt(m_renderArrays.Colours.Count - 1);
+            }
         }
 
         public void Tick()
         {
+            countdown--;
+
+            if (countdown < 0)
+            {
+                countdown = 1;
+                activeList++;
+
+                if (activeList >= m_positions.Count)
+                {
+                    activeList = 0;
+                }
+
+                SwitchActiveList(activeList);
+            }
         }
     }
 }
